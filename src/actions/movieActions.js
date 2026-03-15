403e438167ef7d0c00c77f363440e3631f5bf01d
SamFreddy <sam.fredrick@ucdenver.edu>
2026-03-15 12:16:21 -0600

    Fix bearer token in React movie requests

--- src/actions/movieActions.js
+++ src/actions/movieActions.js
@@ -36,7 +36,7 @@ export function fetchMovie(movieId) {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('token')
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
             },
             mode: 'cors'
         }).then((response) => {
@@ -57,7 +57,7 @@ export function fetchMovies() {
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
-                'Authorization': localStorage.getItem('token')
+                'Authorization': 'Bearer ' + localStorage.getItem('token')
             },
             mode: 'cors'
         }).then((response) => {
