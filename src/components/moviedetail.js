5b115f97e702ae34b5844cdb6723a3a6252d2a32
SamFreddy <sam.fredrick@ucdenver.edu>
2026-03-15 12:38:26 -0600

    Simplify movie detail display

--- src/components/moviedetail.js
+++ src/components/moviedetail.js
@@ -1,79 +1,53 @@
 import React, { useEffect } from 'react';
 import { fetchMovie } from '../actions/movieActions';
 import { useDispatch, useSelector } from 'react-redux';
-import { Card, ListGroup, ListGroupItem, Image } from 'react-bootstrap';
-import { BsStarFill } from 'react-icons/bs';
+import { Card, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 const MovieDetail = () => {
   const dispatch = useDispatch();
   const { movieId } = useParams();
   const selectedMovie = useSelector(state => state.movie.selectedMovie);
-  const loading = useSelector(state => state.movie.loading);
-  const error = useSelector(state => state.movie.error);
 
   useEffect(() => {
     dispatch(fetchMovie(movieId));
   }, [dispatch, movieId]);
 
-  const DetailInfo = () => {
-    if (loading) {
-      return <div>Loading....</div>;
-    }
+  if (!selectedMovie) {
+    return <div>No movie data available.</div>;
+  }
 
-    if (error) {
-      return <div>Error: {error}</div>;
-    }
+  return (
+    <Card className="bg-dark text-white p-4 rounded m-3">
+      <Card.Header>Movie Detail</Card.Header>
 
-    if (!selectedMovie) {
-      return <div>No movie data available.</div>;
-    }
+      <ListGroup>
+        <ListGroupItem><b>Title:</b> {selectedMovie.title}</ListGroupItem>
+        <ListGroupItem><b>Genre:</b> {selectedMovie.genre}</ListGroupItem>
+        <ListGroupItem><b>Release Date:</b> {selectedMovie.releaseDate}</ListGroupItem>
+        <ListGroupItem>
+          <b>Actors:</b>
+          {(selectedMovie.actors || []).map((actor, i) => (
+            <p key={i} className="mb-1">
+              {actor.actorName} as {actor.characterName}
+            </p>
+          ))}
+        </ListGroupItem>
+      </ListGroup>
 
-    return (
-      <Card className="bg-dark text-white p-4 rounded">
-        <Card.Header>Movie Detail</Card.Header>
-
-        {selectedMovie.imageUrl && (
-          <Card.Body>
-            <Image className="image" src={selectedMovie.imageUrl} thumbnail />
-          </Card.Body>
+      <Card.Body className="bg-white text-dark">
+        {(selectedMovie.reviews || []).length > 0 ? (
+          selectedMovie.reviews.map((review, i) => (
+            <p key={i}>
+              <b>{review.username}</b>: {review.review} ({review.rating})
+            </p>
+          ))
+        ) : (
+          <p>No reviews available.</p>
         )}
-
-        <ListGroup>
-          <ListGroupItem>{selectedMovie.title}</ListGroupItem>
-          <ListGroupItem>Genre: {selectedMovie.genre}</ListGroupItem>
-          <ListGroupItem>Release Date: {selectedMovie.releaseDate}</ListGroupItem>
-          <ListGroupItem>
-            {(selectedMovie.actors || []).map((actor, i) => (
-              <p key={i}>
-                <b>{actor.actorName}</b> {actor.characterName}
-              </p>
-            ))}
-          </ListGroupItem>
-          <ListGroupItem>
-            <h4>
-              <BsStarFill /> {selectedMovie.avgRating || 'N/A'}
-            </h4>
-          </ListGroupItem>
-        </ListGroup>
-
-        <Card.Body className="card-body bg-white text-dark">
-          {(selectedMovie.reviews || []).length > 0 ? (
-            selectedMovie.reviews.map((review, i) => (
-              <p key={i}>
-                <b>{review.username}</b>&nbsp; {review.review} &nbsp; <BsStarFill />{' '}
-                {review.rating}
-              </p>
-            ))
-          ) : (
-            <p>No reviews available.</p>
-          )}
-        </Card.Body>
-      </Card>
-    );
-  };
-
-  return <DetailInfo />;
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default MovieDetail;
